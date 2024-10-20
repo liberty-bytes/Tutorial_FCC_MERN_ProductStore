5b9c46784e576c910613bbf14b6a664ba464be0d
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2024-10-20 22:16:38 +0100

    MongoDB/Mongoose schema created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./config/db.js";
+import Product from "./databaseModels/product.model.js";
 
 dotenv.config();
 
@@ -11,6 +12,26 @@ app.get("/", function (req, res) {
   res.send("Server is ready");
 });
 app.get("/products", (req, res) => {});
+
+app.post("/products", async (req, res) => {
+  const product = req.body; //user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all required fields." });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 console.log("LOG: ", process.env.MONGO_URI);
 
