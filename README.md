06f232ee49f79f755e9c8cd36f4d0fc62f64f20a
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2024-10-20 18:43:45 +0100

    Connecting to MongoDB works correctly

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ YT: https://www.youtube.com/watch?v=O3BUHwfHf84
 
 Command to run backend locally from ROOT: npm run backend
 
+<!-- Script in package.json
+nodemon backend/server.j
+to run backend -->
+
 ![image](https://github.com/user-attachments/assets/9a97b8c3-c1e5-401e-b478-c0ca8851d660)
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+// const express = require('express'); // OLD WAY CommonJS, NEWER WAY is Module
 import express from "express";
+import dotenv from "dotenv";
+import { connectToDatabase } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
+app.get("/", function (req, res) {
+  res.send("Server is ready");
 });
+app.get("/products", (req, res) => {});
+
+console.log("LOG: ", process.env.MONGO_URI);
 
 app.listen(5000, () => {
-  console.log(`Server started on http://localhost5000`);
+  connectToDatabase();
+  console.log(`Server started on http://localhost:5000`);
 });
